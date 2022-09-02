84123a1a7073627eef9b79ebac9cf5bd1233285c
Eshka-jpg <ivan_titov_2000@bk.ru>
2022-09-02 22:08:24 +0300

    Added a drone. A little tidying up in the folders. An example of a sound wave is made.

--- Assets/Scripts/Actors/OnSiteFlight.cs
+++ Assets/Scripts/Actors/OnSiteFlight.cs
@@ -6,21 +6,25 @@ public class OnSiteFlight : MonoBehaviour
     [SerializeField] private float secondsFlight;
     [SerializeField] private float amplitude;
     
-    private Vector3 _startPosition;
-
+    private Vector3 _startPoint;
+    
     private void Start()
     {
-        _startPosition = transform.position;
-        transform.position = EndPosition(Vector3.down);
-        
+        _startPoint = transform.localPosition;
+        transform.localPosition = EndPosition(Vector3.down);
         var sequence = DOTween.Sequence();
-        sequence.Append(transform.DOMove(EndPosition(Vector3.up), secondsFlight));
-        sequence.Append(transform.DOMove(EndPosition(Vector3.down), secondsFlight));
+        FlightUpToDown(sequence);
+    }
+
+    private void FlightUpToDown(Sequence sequence)
+    {
+        sequence.Append(transform.DOLocalMove(EndPosition(Vector3.up), secondsFlight));
+        sequence.Append(transform.DOLocalMove(EndPosition(Vector3.down), secondsFlight));
         sequence.SetLoops(-1, LoopType.Restart);
     }
 
     private Vector3 EndPosition(Vector3 direction)
     {
-        return new Vector3(0,_startPosition.y + amplitude * direction.y,0);
+        return _startPoint + new Vector3(0,direction.y * amplitude,0);
     }
 }
