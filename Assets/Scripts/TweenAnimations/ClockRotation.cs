3a7ae99e4a1afbaad53f9468306a951022211942
Yockomo <Funohs@yandex.ru>
2022-09-04 20:08:16 +0300

    clock rotation script added

--- Assets/Scripts/TweenAnimations/ClockRotation.cs
+++ Assets/Scripts/TweenAnimations/ClockRotation.cs
@@ -7,19 +7,23 @@ public class ClockRotation : MonoBehaviour
     [Header("Animation Settings")] 
     [SerializeField] private float _delayBeforeStart = 0;
     [SerializeField] private float _loopDurationInSeconds;
+    [SerializeField] private Vector3 _endValue;
     
     private void Start()
     {
-        var endValue = new Quaternion(0,360,0,0);
-        var sequence = DOTween.Sequence();
-        sequence.Append(transform.DORotateQuaternion(endValue, _loopDurationInSeconds));
-        
-        StartCoroutine(StartAsync(sequence));
+        StartCoroutine(StartAsync());
     }
 
-    private IEnumerator StartAsync(Sequence sequence)
+    private IEnumerator StartAsync()
     {
         yield return new WaitForSecondsRealtime(_delayBeforeStart);
-        sequence.SetLoops(-1, LoopType.Restart);
+        StartLoop();
+    }
+
+    private void StartLoop()
+    {
+        var sequence = DOTween.Sequence();
+        sequence.Append(transform.DORotate(_endValue, _loopDurationInSeconds));
+        sequence.SetLoops(-1, LoopType.Yoyo);
     }
 }
