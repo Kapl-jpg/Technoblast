01c3e657a89b6bcd3817e05d06655d86276f38b4
DOFR <xwolfdofr@gmail.com>
2022-09-06 01:29:13 +0300

    Revert "Change jump"
    This reverts commit 9189fcd33ffcf9446ba558171e579ba06c8a1ed0.

--- Assets/Scripts/Behaviour/Player/SoundWave.cs
+++ Assets/Scripts/Behaviour/Player/SoundWave.cs
@@ -24,61 +24,14 @@ public class SoundWave : BaseBehaviour
     private Rigidbody _currentRigidbody;
     [SerializeField] private Vector3 boxSize;
 
-    [SerializeField] private BoxCollider rightBoxCollider;
-    [SerializeField] private BoxCollider leftBoxCollider;
-    [SerializeField] private BoxCollider upBoxCollider;
-    [SerializeField] private BoxCollider downBoxCollider;
-
     public event Action<JumpableObjectData> JumpableObjectHitEvent;
     public event Action JumpableObjectMissEvent;
 
-    public bool stayOnPlatform { get; set; }
-
-    public Collider Collider { get; set; }
-
-    public Vector3 forceDirection { get; set; }
     private void Start()
     {
-        SetSizeBoxCollider();
-        SetPositionBoxCollider(Vector3.right);
-        SetPositionBoxCollider(Vector3.left);
-        SetPositionBoxCollider(Vector3.up);
-        SetPositionBoxCollider(Vector3.down);
-        
         _playerInput = GetComponent<InputHandler>();
         _currentRigidbody = GetComponent<Rigidbody>();
         _launchWaveVisual = GetComponent<LaunchWaveVisual>();
-    }
-
-    private void SetSizeBoxCollider()
-    {
-        rightBoxCollider.size = boxSize;
-        leftBoxCollider.size = boxSize;
-        upBoxCollider.size = boxSize;
-        downBoxCollider.size = boxSize;
-    }
-
-    private void SetPositionBoxCollider(Vector3 direction)
-    {
-        CurrentBoxCollider(direction).gameObject.transform.position = CurrentRay(direction).direction * (GetDistance(direction) / 2);
-        SetBoxColliderRotation(direction);
-    }
-
-    private void SetBoxColliderRotation(Vector3 direction)
-    {
-        CurrentBoxCollider(direction).gameObject.transform.LookAt(CurrentRay(direction).direction);
-    }
-    
-    private Collider CurrentBoxCollider(Vector3 direction)
-    {
-        return direction switch
-        {
-            var v when v.Equals(Vector3.down) => downBoxCollider,
-            var v when v.Equals(Vector3.up) => upBoxCollider,
-            var v when v.Equals(Vector3.left) => leftBoxCollider,
-            var v when v.Equals(Vector3.right) => rightBoxCollider,
-            _ => null
-        };
     }
 
     protected override void OnUpdate()
@@ -88,35 +41,30 @@ public class SoundWave : BaseBehaviour
 
     private void Flight(Vector3 direction)
     {
-        if (direction == Vector3.zero) 
-            return;
-
-        Collider = CurrentBoxCollider(direction).GetComponent<CollidersHit>().GetCollider();
-        
-        if (stayOnPlatform && Collider.TryGetComponent<IJumpableObject>(out var objectData))
+        if (direction != Vector3.zero)
         {
-            JumpableObjectHitEvent?.Invoke(objectData.GetData());
-            AddForce(GetForceDirection(direction), objectData.GetData().ObjectForce);
-        }
-        else
-        {
-            JumpableObjectMissEvent?.Invoke();
-        }
 
-        _launchWaveVisual.Launch(CurrentRay(direction).direction);
+            // if(Physics.BoxCast(CurrentRay(direction).GetPoint(0.5f), boxSize / 2,
+            //           CurrentRay(direction).direction, out var hit) &&
+            //       hit.collider.TryGetComponent<IJumpableObject>(out var objectData))
+            if (Physics.Raycast(CurrentRay(direction), out var hit, _currentDistance) &&
+                hit.collider.TryGetComponent<IJumpableObject>(out var objectData))
+            {
+                JumpableObjectHitEvent?.Invoke(objectData.GetData());
+                AddForce(GetForceDirection(hit.point), objectData.GetData().ObjectForce);
+            }
+            else
+            {
+                JumpableObjectMissEvent?.Invoke();
+            }
+
+            _launchWaveVisual.Launch(CurrentRay(direction).direction);
+        }
     }
 
-    private Vector3 GetForceDirection(Vector3 direction)
+    private Vector3 GetForceDirection(Vector3 hitPoint)
     {
-        var position = transform.position;
-        return direction switch
-        {
-            var v when v.Equals(Vector3.down) => (position - downBoxCollider.transform.position).normalized,
-            var v when v.Equals(Vector3.up) => (position - upBoxCollider.transform.position).normalized,
-            var v when v.Equals(Vector3.left) => (position - leftBoxCollider.transform.position).normalized,
-            var v when v.Equals(Vector3.right) => (position - rightBoxCollider.transform.position).normalized,
-            _ => Vector3.zero
-        };
+        return (transform.position - hitPoint).normalized;
     }
 
     private void AddForce(Vector3 direction, float forceValue)
@@ -126,7 +74,21 @@ public class SoundWave : BaseBehaviour
 
         _currentRigidbody.velocity =
             direction.x != 0 ? new Vector3(0, 0) : new Vector3(_currentRigidbody.velocity.x, 0);
+
         _currentRigidbody.AddForce(direction * forceValue, ForceMode.Impulse);
+    }
+
+    #region Get box cast
+
+    private Vector3 CenterBox(Vector3 direction)
+    {
+        var center = CurrentRay(direction).direction * GetDistance(direction)/2;
+        return center;
+    }
+
+    private void LookAt(Vector3 direction)
+    {
+        transform.LookAt(direction);
     }
 
     private float GetDistance(Vector3 direction)
@@ -140,6 +102,8 @@ public class SoundWave : BaseBehaviour
             _ => 0
         };
     }
+
+    #endregion
 
     #region Get rays
 
@@ -212,8 +176,7 @@ public class SoundWave : BaseBehaviour
         Debug.DrawRay(position, new Vector3(-CosValue(), SinValue()) * rayLeftDistance, Color.yellow);
         Debug.DrawRay(position, Vector3.down * rayDownDistance, Color.yellow);
         Debug.DrawRay(position, Vector3.up * rayUpDistance, Color.yellow);
-
     }
-    
+
     #endregion
 }
