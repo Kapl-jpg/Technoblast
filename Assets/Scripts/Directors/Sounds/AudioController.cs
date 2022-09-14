a1f97097b3934301e8e535f3b264835b48946315
Yockomo <Funohs@yandex.ru>
2022-09-15 00:38:15 +0300

    Sound Settings added

--- Assets/Scripts/Directors/Sounds/AudioController.cs
+++ Assets/Scripts/Directors/Sounds/AudioController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using ScriptableObjects.Sounds_SO;
 using UnityEngine;
 
@@ -7,7 +7,7 @@ namespace Directors
     public class AudioController : MonoBehaviour
     {
         [SerializeField] private Profiles _profiles;
-
+        [SerializeField] private List<MusicSlider> _volumeSlider = new List<MusicSlider>();
         public Profiles Profile
         {
             get => _profiles;
@@ -15,8 +15,31 @@ namespace Directors
 
         private void Awake()
         {
-            if(_profiles != null)
-                Profile = _profiles;
+            if(_profiles !=null)
+                _profiles.SetProfile(_profiles);
+        }
+
+        private void Start()
+        {
+            if(Settings.Profile && Settings.Profile.AudioMixer != null)
+                Settings.Profile.GetAudioLevels();
+        }
+
+        public void ApplyChanges()
+        {
+            if(Settings.Profile && Settings.Profile.AudioMixer != null)
+                Settings.Profile.SaveAudioLevels();
+        }
+
+        public void CancelChanges()
+        {
+            if(Settings.Profile && Settings.Profile.AudioMixer != null)
+                Settings.Profile.GetAudioLevels();
+            
+            for (int i = 0; i < _volumeSlider.Count; i++)
+            {
+                _volumeSlider[i].ResetSliderValue();
+            }
         }
     }
 }
--- Assets/Scripts/Directors/UI/SoundsSettings/MusicSlider.cs
+++ Assets/Scripts/Directors/UI/SoundsSettings/MusicSlider.cs
@@ -1,21 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using ScriptableObjects.Sounds_SO;
 using UnityEngine;
-using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class MusicSlider : MonoBehaviour
 {
     [SerializeField] private string _volumeName;
-    [SerializeField] private AudioMixer _audioMixer;
+
+    public Slider _slider { get; set; }
+    
+    
+    
+    private void Start()
+    {
+        _slider = GetComponent<Slider>();
+        ResetSliderValue();
+        UpdateValueOnChange(_slider.value);
+        _slider.onValueChanged.AddListener(delegate
+        {
+            UpdateValueOnChange(_slider.value);
+        });
+    }
 
     public void UpdateValueOnChange(float value)
     {
-        if (Settings.Profile && Settings.Profile.AudioMixer)
+        if (Settings.Profile)
         {
-            var volume = Mathf.Log(value) * 20f;
-            Settings.Profile.AudioMixer.SetFloat(_volumeName, volume);
+             Settings.Profile.SetAudioLevels(_volumeName,value);
+        }
+    }
+
+    public void ResetSliderValue()
+    {
+        if (Settings.Profile)
+        {
+            var volume = Settings.Profile.GetAudioLevels(_volumeName);
+            
+            UpdateValueOnChange(volume);
+            GetComponent<Slider>().value = volume;
         }
     }
 }
