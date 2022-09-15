b91baa38c429b7a6ae9df39b27a118a568c0ae57
Kapl-jpg <ivan_titov_2000@bk.ru>
2022-09-15 19:24:23 +0300

    Added background to levels 11- 18. Change out of portal animation.

--- Assets/Scripts/ParallaxEffect.cs
+++ Assets/Scripts/ParallaxEffect.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
+using Zenject;
 
 public class ParallaxEffect : MonoBehaviour
 {
-    [SerializeField] private Transform characterTransform;
     [SerializeField] private Transform[] layers;
     [SerializeField] private float[] ratio;
     [SerializeField] private Vector3 offset;
+
+    private CharacterMovement _characterMovement;
+
+    [Inject]
+    private void Construct(CharacterMovement characterMovement)
+    {
+        _characterMovement = characterMovement;
+    }
     
     void Update()
     {
         for (int i = 0; i < layers.Length; i++)
         {
-            var position = characterTransform.position;
+            var position = _characterMovement.transform.position;
             layers[i].position = new Vector3(position.x * ratio[i] + offset.x, position.y * ratio[i] + offset.y, offset.z);
         }
     }
