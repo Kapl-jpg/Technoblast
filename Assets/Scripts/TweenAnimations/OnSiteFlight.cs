608cdce22266380db96970430fcac0f625b9cc0c
Kapl-jpg <ivan_titov_2000@bk.ru>
2023-01-01 17:58:34 +0300

    Change PlatformFlight script

--- Assets/Scripts/TweenAnimations/OnSiteFlight.cs
+++ Assets/Scripts/TweenAnimations/OnSiteFlight.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using DG.Tweening;
 
@@ -6,14 +5,14 @@ public class OnSiteFlight : MonoBehaviour
 {
     [SerializeField] private float secondsFlight;
     [SerializeField] private float amplitude;
-    
+    [SerializeField] private float maxOffsetTime = 0.2f;
+
     private Vector3 _startPoint;
     private Sequence _sequence;
-    
+
     private void Start()
     {
-        _startPoint = transform.localPosition;
-        transform.localPosition = EndPosition(Vector3.down);
+        
         _sequence = DOTween.Sequence();
         FlightUpToDown(_sequence);
     }
@@ -25,13 +24,27 @@ public class OnSiteFlight : MonoBehaviour
 
     private void FlightUpToDown(Sequence sequence)
     {
-        sequence.Append(transform.DOLocalMove(EndPosition(Vector3.up), secondsFlight));
-        sequence.Append(transform.DOLocalMove(EndPosition(Vector3.down), secondsFlight));
+        var rand = Random.Range(0, 2);
+        if (rand == 0)
+        {
+            _startPoint = transform.localPosition;
+            transform.localPosition = EndPosition(Vector3.down);
+            sequence.Append(transform.DOLocalMove(EndPosition(Vector3.up), secondsFlight + Random.Range(-maxOffsetTime,maxOffsetTime)));
+            sequence.Append(transform.DOLocalMove(EndPosition(Vector3.down), secondsFlight+ Random.Range(-maxOffsetTime,maxOffsetTime)));
+        }
+        else
+        {
+            _startPoint = transform.localPosition;
+            transform.localPosition = EndPosition(Vector3.up);
+            sequence.Append(transform.DOLocalMove(EndPosition(Vector3.down), secondsFlight+ Random.Range(-maxOffsetTime,maxOffsetTime)));
+            sequence.Append(transform.DOLocalMove(EndPosition(Vector3.up), secondsFlight+ Random.Range(-maxOffsetTime,maxOffsetTime)));
+        }
+
         sequence.SetLoops(-1, LoopType.Restart);
     }
 
     private Vector3 EndPosition(Vector3 direction)
     {
-        return _startPoint + new Vector3(0,direction.y * amplitude,0);
+        return _startPoint + new Vector3(0, direction.y * amplitude, 0);
     }
 }
